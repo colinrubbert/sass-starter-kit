f9acdb2cd9c0f7664e71e41737bafad0bd7423a4
Colin Rubbert <colinrubbert@gmail.com>
2017-03-10 15:16:43 -0600

    Enhance Sass Starter Kit with mobile menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Main JavaScript File
+
+// Toggle Mobile Navigation Menu
+
+var mobileMenu = document.getElementById("mobile-navigation-wrapper").classList;
+
+function toggleMenu() {
+  mobileMenu.toggle("open");
+}
